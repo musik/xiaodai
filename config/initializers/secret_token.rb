40f4015d825550262d612ecc03778f8dd69e985b
musik <860577@gmail.com>
2013-03-24 15:31:33 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xiaodai::Application.config.secret_token = '2f74a0b52232b112dc7e6cdb5ea516b98da0f8939a624c0995b83a5b95c02555d9d07f5854cc25c01e41320aba6702786cc21065d6ceb7f26b6a225ccdb936dd'
